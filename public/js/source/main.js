04aa44c37724e6355e0d4773b27a5a166eceeb69
markredballoon <mark@redballoondesign.co.uk>
2017-06-09 16:04:26 +0100

    updates to the plugin js. removed cdn fallback

--- public/js/source/main.js
+++ public/js/source/main.js
@@ -13,12 +13,16 @@ var videoBaseScript = require('./video-base.js');
     }
 
     if(typeof jQuery =='undefined') {
+        // This contrevens the development guidlines, but is useful if developing prototypes outside of wordpress:
+        /*
         var headTag = document.getElementsByTagName("head")[0];
         var jqTag = document.createElement('script');
         jqTag.type = 'text/javascript';
         jqTag.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js';
         jqTag.onload = jqueryLoaded;
         headTag.appendChild(jqTag);
+        */
+        console.log("jQuery not loaded.");
     } else {
         jqueryLoaded();
     }
